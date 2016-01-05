c71b4f1570f3f4f9cb8c9d31650d8d9715cb9fc5
acareaga <aaroncareaga@gmail.com>
2016-01-04 18:44:24 -0700

    Find for merchants is working

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -9,7 +9,11 @@ class Api::V1::MerchantsController < ApplicationController
     respond_with Merchant.find_by(id: params[:id])
   end
 
-  def find(params)
-    respond_with Merchant.find_by(params)
+  def find
+    if params.first.first == "id"
+      respond_with Merchant.find(params.first.last.to_i)
+    else
+      respond_with Merchant.where("#{params.first.first} LIKE ?", params.first.last)
+    end
   end
 end
