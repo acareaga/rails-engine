75569991fc93ae7d2475a093a7b4623b84506d77
acareaga <aaroncareaga@gmail.com>
2016-01-05 08:47:19 -0700

    Added format method in item and invoice item models

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,8 +1,15 @@
 class InvoiceItem < ActiveRecord::Base
   belongs_to :item
   belongs_to :invoice
+  before_save :format_currency
 
   def self.random
     order("RANDOM()").first
+  end
+
+  private
+
+  def format_currency
+    self.unit_price = unit_price/100.00
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,15 @@ class Item < ActiveRecord::Base
   belongs_to :merchant
   belongs_to :invoice
   has_many :invoice_items
+  before_save :format_currency
 
   def self.random
     order("RANDOM()").first
+  end
+
+  private
+
+  def format_currency
+    self.unit_price = unit_price/100.00
   end
 end
