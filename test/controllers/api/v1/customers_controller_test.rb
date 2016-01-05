e732df104aa20b8bad40cfefb2593a62d7d37438
acareaga <aaroncareaga@gmail.com>
2016-01-05 13:17:52 -0700

    Fixed currency format and working on controller tests

--- test/controllers/api/v1/customers_controller_test.rb
+++ test/controllers/api/v1/customers_controller_test.rb
@@ -1,33 +1,82 @@
 require 'test_helper'
 
 class Api::V1::CustomersControllerTest < ActionController::TestCase
-  test '#index returns all customers' do
+
+  test '#index responds to json' do
     get :index, format: :json
     assert_response :success
   end
 
-  test '#show returns a specific customer by id' do
-    get :show, format: :json, id: 10
+  test '#index returns an array of records' do
+    get :index, format: :json
+    assert_kind_of Array, json_response
+  end
+
+  test '#index returns the correct number of customers' do
+    get :index, format: :json
+    assert_equal Customer.count, json_response.count
+  end
+
+  test '#index contains items that have the correct properties' do
+    get :index, format: :json
+
+    json_response.each do |item|
+      assert item["first_name"]
+      assert item["last_name"]
+    end
+  end
+
+  test '#show responds to json' do
+    get :show, format: :json, id: Customer.last.id
     assert_response :success
   end
 
-  test '#find returns a customer by specific case-insensitive first name' do
-    get :find, format: :json, first_name: "sHaWn"
+  test '#show returns a single record' do
+    get :show, format: :json, id: Customer.last.id
+    assert_kind_of Hash, json_response
+  end
+
+  test '#show returns the correct item' do
+    get :show, format: :json, id: Customer.last.id
+    assert_equal Customer.last.id, json_response["id"]
+  end
+
+  test '#find responds to json' do
+    get :find, format: :json, first_name: Customer.last.first_name
     assert_response :success
   end
 
-  test '#find returns a customer by specific case-insensitive last name' do
-    get :find, format: :json, last_name: "lAnGwOrTh"
+  test '#find returns a single record' do
+    get :find, format: :json, first_name: Customer.last.first_name
+    assert_kind_of Hash, json_response
+  end
+
+  test '#find returns the correct item' do
+    get :find, format: :json, id: Customer.first.id
+    assert_equal Customer.first.id, json_response["id"]
+  end
+
+  test '#find_all responds to json' do
+    get :find_all, format: :json, first_name: Customer.last.first_name
     assert_response :success
   end
 
-  # test '#find returns a customer by specific id' do
-  #   get :find, format: :json, 'id': 54
-  #   assert_response :success
-  # end
+  test '#find_all returns all matching records' do
+    get :find_all, format: :json, first_name: Customer.last.first_name
+    assert_equal 2, json_response.count
+  end
 
-  test '#random returns a random customer' do
-    get :random, format: :json
+  ##### WHAT ABOUT RANDOM??
+
+  test '#invoices responds to json' do
+    get :invoices, format: :json, id: Customer.last.id
     assert_response :success
   end
+
+  test '#invoices returns a specific customers records' do
+    get :invoices, format: :json, id: Customer.first.id
+    binding.pry
+    assert_equal 1, json_response.count
+  end
+
 end
