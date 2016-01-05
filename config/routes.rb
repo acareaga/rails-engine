8b5d6c2d6a882ab2c76af7cf2f57d5acc6e252b8
acareaga <aaroncareaga@gmail.com>
2016-01-04 17:47:44 -0700

    Started find for merchants

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-        resources :merchants,     except: [:new, :edit], defaults: { format: :json }
-        resources :customers,     except: [:new, :edit], defaults: { format: :json }
-        resources :items,         except: [:new, :edit], defaults: { format: :json }
-        resources :invoices,      except: [:new, :edit], defaults: { format: :json }
-        resources :invoice_items, except: [:new, :edit], defaults: { format: :json }
-        resources :transactions,  except: [:new, :edit], defaults: { format: :json }
+        resources :merchants,     only: [:index, :show, :find], defaults: { format: :json }
+        resources :customers,     only: [:index, :show, :find], defaults: { format: :json }
+        resources :items,         only: [:index, :show, :find], defaults: { format: :json }
+        resources :invoices,      only: [:index, :show, :find], defaults: { format: :json }
+        resources :invoice_items, only: [:index, :show, :find], defaults: { format: :json }
+        resources :transactions,  only: [:index, :show, :find], defaults: { format: :json }
     end
   end
 end
