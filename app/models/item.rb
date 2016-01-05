f727353cf674607c5870b663616c7cba846c8cab
acareaga <aaroncareaga@gmail.com>
2016-01-05 08:29:07 -0700

    Added item relationship end points

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :merchant
   belongs_to :invoice
-  belongs_to :invoice_items
+  has_many :invoice_items
 
   def self.random
     order("RANDOM()").first
